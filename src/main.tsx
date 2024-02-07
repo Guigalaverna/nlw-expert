261eb7f7b50d1281ec141892e692ab667b281388
Guilherme Galaverna <guigalaverna@gmail.com>
2024-02-07 12:45:54 -0300

    feat: add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./app";
 import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1></h1>
+    <App />
   </React.StrictMode>
 );
